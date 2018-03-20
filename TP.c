411f07be93dd7e04109940695ea700e1dd4f9a94
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-03-20 16:47:18 -0600

    Se genera la matriz y el vector iniciales

--- TP.c
+++ TP.c
@@ -1,14 +1,33 @@
-#include "stdio.h"
+#include <stdio.h>
+#inlcude <stdlib.h>
 
 int main() {
 
-	int n;
-
-	printf("Inserte el n√mero de filas/columnas: ");
+	int n, i, j;
+	int* M, v;
+	srand(time(NULL));
+	
+	printf("Inserte el numero de filas/columnas: ");
 	scanf("%d", &n);
 
-
+	M = (int*) malloc(n * n * sizeof(int));
+	v = (int*) malloc(n " sizeof(int));
+			  
+	//Se generan M y v
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			M[i][j] = rand();	
+		}
+		v[i] = rand();
+	}
+			  
+	//magia
+	magia();
+			  
+	//desplegar resultados
 	
-
+	
+	free(M);
+	free(v);
 	return 0;
 }
