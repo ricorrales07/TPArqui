2b65d5c1bda27109ab10ad0c7701e59d5f14d666
ricorrales07 <ricorrales07@gmail.com>
2018-04-06 10:43:48 -0600

    Ya compila (no corre, ver TODOs)

--- TP.c
+++ TP.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "mpi.h"
 
-int* repartirFilas(int* M, int filas, int numprocs, int myid, int* filas_por_proceso);
-int* calcularQ(int* M, int* v, int myid);
-void calcularP(int* M_porcion, int myid, int numprocs, int* filas_por_proceso, int n, int* P);
-int calcularTp(int* P, int n);
+int *repartirFilas(int *M, int filas, int numprocs, int myid, int *filas_por_proceso);
+int *calcularQ(int *M, int* v, int myid);
+void calcularP(int *M_porcion, int myid, int numprocs, int *filas_por_proceso, int n, int *P);
+int calcularTp(int *P, int n);
 
 int i, j, k; //auxiliares para ciclos
 int aux;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
 	/**************************
 	n: tamano de la matriz
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
 	**************************/
 
 	int n, numprocs, myid, Tp;
-	int* M, v, Q, M_porcion, filas_por_proceso, P;
+	int *M, *v, *Q, *M_porcion, *filas_por_proceso, *P;
 	srand(time(NULL));
 	
 	MPI_Init(&argc, &argv);
@@ -36,20 +36,36 @@ int main(int argc, char** argv) {
 		printf("Inserte el numero de filas/columnas (múltiplo de %d): ", numprocs);
 		scanf("%d", &n);
 		//TODO: revisar que de verdad sea un múltiplo
+		//TODO: ENVIAR n A LOS OTROS PROCESOS
 
-		M = (int*) malloc(n * n * sizeof(int)); //tal vez sea mejor alocar por filas (mejor no; complica el scatterv)
-		v = (int*) malloc(n * sizeof(int));
+		M = (int *) malloc(n * n * sizeof(int)); //tal vez sea mejor alocar por filas (mejor no; complica el scatterv)
+		v = (int *) malloc(n * sizeof(int));
 
 		//TODO: revisar que no sean NULL
 
 		//Se generan M y v
 		for (i = 0; i < n; i++) {
 			for (j = 0; j < n; j++) {
-				M[i][j] = rand() % 10;	
+				*(M + i * n + j) = rand() % 10;	//aritmética de punteros porque es más fácil de entender que un puntero de punteros
 			}
 			v[i] = rand() % 6;
 		} //end for
+
+		//Imprimimos M y v
+		printf("Matriz M:\n");
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				printf("%d ", *(M + i * n + j));	
+			}
+			printf("\n");
+		} //end for
+		printf("\nVector v:\n");
+		for (i = 0; i < n; i++)
+			printf("%d ", v[i]);
 	} //end if
+
+	//TODO: RECIBIR n
+	//TODO: ESPERAR AL PROCESO 0
 
 	//cada proceso tiene un vector P para resultados parciales
 	P = (int*) malloc(n * sizeof(int));
@@ -58,13 +74,13 @@ int main(int argc, char** argv) {
 		P[i] = 0;
 
 	//repartir las filas de la matriz
-	M_porcion = repartirFilas(M, n, numprocs, myid);
+	M_porcion = repartirFilas(M, n, numprocs, myid, filas_por_proceso);
 		
 	//calcular Q = Mv
 	//Q = calcularQ(M, v, myid);
 
 	//calcular P tal que P[i] = cantidad de primos en la columna i de M
-	calcularP(M_porcion, myid, numprocs, filas_por_proceso, n);
+	calcularP(M_porcion, myid, numprocs, filas_por_proceso, n, P);
 	
 	//calcular tp = número total de primos en M
 	if (myid == 0)
@@ -73,13 +89,18 @@ int main(int argc, char** argv) {
 	//calcular matriz B (suma de elementos en M; ver dibujo en pdf)
 	//calcularB(M);
 			  
-	//desplegar resultados
-	
 	if (myid == 0) {
+		//desplegar resultados
+		printf("\nVector P:\n");
+		for (i = 0; i < n; i++)
+			printf("%d ", P[i]);
+		printf("\nTp = %d", Tp);
+
 		free(M);
 		free(v);
 	}
 
+	free(M_porcion);
 	free(P);
 	
 	MPI_Finalize();
@@ -87,7 +108,7 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-int* repartirFilas(int* M, int n, int numprocs, int myid, int* filas_por_proceso) {	
+int *repartirFilas(int *M, int n, int numprocs, int myid, int *filas_por_proceso) {	
 
 	/****************************
 	sendcounts: cuántos elementos le tocan a cada proceso
@@ -96,7 +117,7 @@ int* repartirFilas(int* M, int n, int numprocs, int myid, int* filas_por_proceso
 	TODO: revisar si esta info se ocupa más adelante y sacarla de acá si es necesario
 	****************************/
 	int sendcounts[numprocs], displs[numprocs], recvcount;
-	int* M_porcion;
+	int *M_porcion;
 
 	*filas_por_proceso = n / numprocs; //no debería dar problemas porque numprocs divide a n
 
@@ -129,7 +150,7 @@ int* repartirFilas(int* M, int n, int numprocs, int myid, int* filas_por_proceso
 		recvcount = (*filas_por_proceso + 2) * n;
 
 	//asignar M_porcion
-	M_porcion = (int*) malloc(recvcount * sizeof(int));
+	M_porcion = (int *) malloc(recvcount * sizeof(int));
 	//TODO: revisar que no sea NULL
 	//TODO: liberar esta memoria
 	
@@ -140,14 +161,17 @@ int* repartirFilas(int* M, int n, int numprocs, int myid, int* filas_por_proceso
 	return M_porcion;
 }
 
-void calcularP(int* M_porcion, int myid, int numprocs, int* filas_por_proceso, int n, int* P) {
+void calcularP(int *M_porcion, int myid, int numprocs, int *filas_por_proceso, int n, int *P) {
 	int primos[4] = {2, 3, 5, 7};
-	int start;
+	int start, end;
+	
+	start = (myid == 0) ? 0 : 1;
+	end = start + *filas_por_proceso;
 
-	for (i = start = (myid == 0) ? 0 : 1; i < start + filas_por_proceso; i++) {
+	for (i = start; i < end; i++) {
 		for (j = 0; j < n; j++) {
 			for (k = 0; k < 4; k++) {
-				if (M_porcion[i][j] == primos[k])
+				if (*(M_porcion + i * n + j) == primos[k])
 					P[j]++;
 			}
 		}
@@ -157,7 +181,7 @@ void calcularP(int* M_porcion, int myid, int numprocs, int* filas_por_proceso, i
 	MPI_Reduce(P, P, n, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 }
 
-int calcularTp(int* P, int n) {
+int calcularTp(int *P, int n) {
 	int accum = 0;
 	for (i = 0; i < n; i++)
 		accum += P[i];
